dc89a3b97b113a11cf19b1878c237df0ab510f41
ahmed14-cell <ahmedito14@gmail.com>
2020-11-11 08:33:06 +0300

    Using random forest

--- produce/train_model.py
+++ produce/train_model.py
@@ -14,8 +14,8 @@ x_train = pd.read_csv(str(Config.features_path / 'train_features.csv'))
 y_train = pd.read_csv(str(Config.features_path / 'train_target.csv'))
 
 # Instantiating and fitting the data with the algorithm
-model = AdaBoostRegressor(DecisionTreeRegressor(max_depth=3),
-                           n_estimators=500, random_state=42)
+model = RandomForestRegressor(n_estimators=500, criterion='mse',
+                                random_state=42, n_jobs=1)
 model = model.fit(x_train, y_train.to_numpy().ravel())
 
 # Saving the model in a pickle file
