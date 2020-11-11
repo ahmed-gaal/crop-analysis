df81a19a1256066662c624f150df8abaed428f82
ahmed14-cell <ahmedito14@gmail.com>
2020-11-11 08:15:33 +0300

    Using Adaboost Regressor

--- produce/train_model.py
+++ produce/train_model.py
@@ -2,7 +2,8 @@ import pickle
 import pandas as pd
 
 from sklearn.linear_model import LinearRegression, ElasticNet
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor
+from sklearn.tree import DecisionTreeRegressor
 from config import Config
 
 # Creating a path to save the models
@@ -13,7 +14,8 @@ x_train = pd.read_csv(str(Config.features_path / 'train_features.csv'))
 y_train = pd.read_csv(str(Config.features_path / 'train_target.csv'))
 
 # Instantiating and fitting the data with the algorithm
-model = LinearRegression()
+model = AdaBoostRegressor(DecisionTreeRegressor(max_depth=3),
+                           n_estimators=500, random_state=42)
 model = model.fit(x_train, y_train.to_numpy().ravel())
 
 # Saving the model in a pickle file
