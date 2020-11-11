dca12f0be1221c735e1c50d60e2ac4c78265cd2e
ahmed14-cell <ahmedito14@gmail.com>
2020-11-11 08:39:45 +0300

    Using Decision Tree

--- produce/train_model.py
+++ produce/train_model.py
@@ -2,7 +2,6 @@ import pickle
 import pandas as pd
 
 from sklearn.linear_model import LinearRegression, ElasticNet
-from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor
 from sklearn.tree import DecisionTreeRegressor
 from config import Config
 
@@ -14,8 +13,7 @@ x_train = pd.read_csv(str(Config.features_path / 'train_features.csv'))
 y_train = pd.read_csv(str(Config.features_path / 'train_target.csv'))
 
 # Instantiating and fitting the data with the algorithm
-model = RandomForestRegressor(n_estimators=500, criterion='mse',
-                                random_state=42, n_jobs=1)
+model = DecisionTreeRegressor(max_depth=3)
 model = model.fit(x_train, y_train.to_numpy().ravel())
 
 # Saving the model in a pickle file
