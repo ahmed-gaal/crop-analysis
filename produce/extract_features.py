46dc1b13853bc990443eaefe957281b1e77abb52
ahmed14-cell <ahmedito14@gmail.com>
2020-11-14 09:28:52 +0300

    Added a preprocessing step

--- produce/extract_features.py
+++ produce/extract_features.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from config import Config
-
+from sklearn.preprocessing import StandardScaler
 # Create a directory to save our features
 Config.features_path.mkdir(parents=True, exist_ok=True)
 
@@ -12,15 +12,28 @@ test_df = pd.read_csv(str(Config.dataset_path / 'test.csv'))
 
 
 def feature_extraction(df):
-    return df[['Area harvested','Yield','Years']]
+    x = df[['Area harvested','Yield','Years']]
+    scaler=StandardScaler()
+    x = scaler.fit_transform(x)
+    return x
 
 
 train_features = feature_extraction(train_df)
 test_features = feature_extraction(test_df)
 
 # Saving the features extracted from above to our directory
-train_features.to_csv(str(Config.features_path / 'train_features.csv'), index=None)
-test_features.to_csv(str(Config.features_path / 'test_features.csv'), index=None)
+pd.DataFrame(train_features).to_csv(str(Config.features_path / 'train_features.csv'), index=None)
+pd.DataFrame(test_features).to_csv(str(Config.features_path / 'test_features.csv'), index=None)
 
-train_df.Production.to_csv(str(Config.features_path / 'train_target.csv'), index=None)
-test_df.Production.to_csv(str(Config.features_path / 'test_target.csv'), index=None)
+def preprocess(df):
+    x = df['Production'].values.reshape(-1, 1)
+    scaler = StandardScaler()
+    return scaler.fit_transform(x)
+
+
+train_target = preprocess(train_df)
+test_target = preprocess(test_df)
+
+# saving to a dataframe
+pd.DataFrame(train_target).to_csv(str(Config.features_path / 'train_target.csv'), index=None)
+pd.DataFrame(test_target).to_csv(str(Config.features_path / 'test_target.csv'), index=None)
--- produce/train_model.py
+++ produce/train_model.py
@@ -2,7 +2,6 @@ import pickle
 import pandas as pd
 
 from sklearn.linear_model import LinearRegression, ElasticNet
-from sklearn.tree import DecisionTreeRegressor
 from config import Config
 
 # Creating a path to save the models
@@ -13,7 +12,7 @@ x_train = pd.read_csv(str(Config.features_path / 'train_features.csv'))
 y_train = pd.read_csv(str(Config.features_path / 'train_target.csv'))
 
 # Instantiating and fitting the data with the algorithm
-model = DecisionTreeRegressor(max_depth=4)
+model = LinearRegression()
 model = model.fit(x_train, y_train.to_numpy().ravel())
 
 # Saving the model in a pickle file
