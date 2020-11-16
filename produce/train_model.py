0af73a9819c5acc5c4db081336db4db23d3480b6
ahmed14-cell <ahmedito14@gmail.com>
2020-11-16 10:08:28 +0300

    Using Adaboost on preprocessed data

--- produce/train_model.py
+++ produce/train_model.py
@@ -1,7 +1,8 @@
 import pickle
 import pandas as pd
 
-from sklearn.linear_model import Ridge
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.tree import DecisionTreeRegressor
 from config import Config
 
 # Creating a path to save the models
@@ -12,7 +13,8 @@ x_train = pd.read_csv(str(Config.features_path / 'train_features.csv'))
 y_train = pd.read_csv(str(Config.features_path / 'train_target.csv'))
 
 # Instantiating and fitting the data with the algorithm
-model = Ridge()
+model = AdaBoostRegressor(DecisionTreeRegressor(max_depth=3),
+                           n_estimators=500, random_state=Config.random_state)
 model = model.fit(x_train, y_train.to_numpy().ravel())
 
 # Saving the model in a pickle file
