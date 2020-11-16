31041226a96904b484538c241d4d02b3c6e7527a
ahmed14-cell <ahmedito14@gmail.com>
2020-11-16 10:13:53 +0300

    Using Adaboost on preprocessed data

--- produce/train_model.py
+++ produce/train_model.py
@@ -13,7 +13,7 @@ x_train = pd.read_csv(str(Config.features_path / 'train_features.csv'))
 y_train = pd.read_csv(str(Config.features_path / 'train_target.csv'))
 
 # Instantiating and fitting the data with the algorithm
-model = AdaBoostRegressor(DecisionTreeRegressor(max_depth=3),
+model = AdaBoostRegressor(DecisionTreeRegressor(max_depth=4),
                            n_estimators=1000, random_state=Config.random_state)
 model = model.fit(x_train, y_train.to_numpy().ravel())
 
