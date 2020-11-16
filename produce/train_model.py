283159575b7ceb84af12da531dbc9ad41e05b402
ahmed14-cell <ahmedito14@gmail.com>
2020-11-16 09:49:04 +0300

    Using Ridge Regularized regression

--- produce/train_model.py
+++ produce/train_model.py
@@ -1,7 +1,7 @@
 import pickle
 import pandas as pd
 
-from sklearn.svm import SVR
+from sklearn.linear_model import Ridge
 from config import Config
 
 # Creating a path to save the models
@@ -12,7 +12,7 @@ x_train = pd.read_csv(str(Config.features_path / 'train_features.csv'))
 y_train = pd.read_csv(str(Config.features_path / 'train_target.csv'))
 
 # Instantiating and fitting the data with the algorithm
-model = SVR(kernel='linear', gamma='auto')
+model = Ridge()
 model = model.fit(x_train, y_train.to_numpy().ravel())
 
 # Saving the model in a pickle file
