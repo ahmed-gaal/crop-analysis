5e06f08fe29cee4409c3877f6fcc5d6255717e5b
ahmed14-cell <ahmedito14@gmail.com>
2020-11-16 10:48:12 +0300

    Using adaboost on Ridge Regression

--- produce/train_model.py
+++ produce/train_model.py
@@ -2,7 +2,7 @@ import pickle
 import pandas as pd
 
 from sklearn.ensemble import AdaBoostRegressor
-from sklearn.svm import SVR
+from sklearn.linear_model import Ridge
 from config import Config
 
 # Creating a path to save the models
@@ -13,7 +13,7 @@ x_train = pd.read_csv(str(Config.features_path / 'train_features.csv'))
 y_train = pd.read_csv(str(Config.features_path / 'train_target.csv'))
 
 # Instantiating and fitting the data with the algorithm
-model = AdaBoostRegressor(SVR(kernel='linear', gamma='auto'), n_estimators=1000,
+model = AdaBoostRegressor(Ridge(), n_estimators=1000,
                           random_state=Config.random_state)
 model = model.fit(x_train, y_train.to_numpy().ravel())
 
