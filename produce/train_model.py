ca3d66e09414cc34e6c31afbeec517ec536585dc
ahmed14-cell <ahmedito14@gmail.com>
2020-11-16 10:12:08 +0300

    Using AdaBoost on preprocessed data

--- produce/train_model.py
+++ produce/train_model.py
@@ -14,7 +14,7 @@ y_train = pd.read_csv(str(Config.features_path / 'train_target.csv'))
 
 # Instantiating and fitting the data with the algorithm
 model = AdaBoostRegressor(DecisionTreeRegressor(max_depth=3),
-                           n_estimators=500, random_state=Config.random_state)
+                           n_estimators=1000, random_state=Config.random_state)
 model = model.fit(x_train, y_train.to_numpy().ravel())
 
 # Saving the model in a pickle file
