85cc753a931ad35baf6978689fbd33f62744e4af
ahmed14-cell <ahmedito14@gmail.com>
2020-11-18 19:13:26 +0300

    Using Adaboost on Gradient-Boosting

--- produce/train_model.py
+++ produce/train_model.py
@@ -1,7 +1,7 @@
 import pickle
 import pandas as pd
 
-from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import GradientBoostingRegressor, AdaBoostRegressor
 from config import Config
 
 # Creating a path to save the models
@@ -12,7 +12,8 @@ x_train = pd.read_csv(str(Config.features_path / 'train_features.csv'))
 y_train = pd.read_csv(str(Config.features_path / 'train_target.csv'))
 
 # Instantiating and fitting the data with the algorithm
-model = GradientBoostingRegressor()
+model = AdaBoostRegressor(base_estimator=GradientBoostingRegressor(),
+                          n_estimators=500, random_state=Config.random_state)
 model = model.fit(x_train, y_train.to_numpy().ravel())
 
 # Saving the model in a pickle file
