6064ae0e1ae7a90fb86f5691ded8c1beddbe6354
ahmed14-cell <ahmedito14@gmail.com>
2020-11-19 10:42:49 +0300

    Experimenting with Random Forest

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # crop-analysis
+This a Reproducible Machine Learning Pipeline using DVC.
+The data used in the development of this ML Pipeline was obtained from the public data portal of the United Nations Food and Agriculture Organisation.
+
+The objective of the model is to predict the Production of Sesame seed when provided with the Area harvested and the Yield.
--- produce/train_model.py
+++ produce/train_model.py
@@ -1,7 +1,7 @@
 import pickle
 import pandas as pd
 
-from sklearn.ensemble import GradientBoostingRegressor, AdaBoostRegressor
+from sklearn.ensemble import RandomForestRegressor
 from config import Config
 
 # Creating a path to save the models
@@ -12,8 +12,9 @@ x_train = pd.read_csv(str(Config.features_path / 'train_features.csv'))
 y_train = pd.read_csv(str(Config.features_path / 'train_target.csv'))
 
 # Instantiating and fitting the data with the algorithm
-model = AdaBoostRegressor(base_estimator=GradientBoostingRegressor(),
-                          n_estimators=500, random_state=Config.random_state)
+model = RandomForestRegressor(
+    max_depth=4, n_estimators=500, random_state=Config.random_state
+)
 model = model.fit(x_train, y_train.to_numpy().ravel())
 
 # Saving the model in a pickle file
